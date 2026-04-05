3b5d4edd5e790c6a4fd8cd6eb0de139acd7ec54b
facuibaquez031997 <facuibaquez03.1997@gmail.com>
2026-04-05 19:15:33 -0300

    arreglos revisados

--- 02-Katas-Clase/arreglos.js
+++ 02-Katas-Clase/arreglos.js
@@ -6,20 +6,20 @@
 // console.log("--- KATA 1: BÚSQUEDA DEL MAYOR ---");
 
 // 🧑‍💻 LA FORMA JUNIOR (El Algoritmo Clásico a mano)
-// let elMayor = numeros[0]; // Asumimos que el primero es el rey provisional
+ let elMayor = numeros[0]; // Asumimos que el primero es el rey provisional
 
-// for (let i = 1; i < numeros.length; i++) {
-//   if (numeros[i] > elMayor) {
-//     elMayor = numeros[i]; // ¡Hay un nuevo rey!
-//   }
-// }
-// console.log("Algoritmo a mano. El mayor es:", elMayor);
+ for (let i = 1; i < numeros.length; i++) {
+   if (numeros[i] > elMayor) {
+     elMayor = numeros[i]; // ¡Hay un nuevo rey!
+   }
+ }
+ console.log("Algoritmo a mano. El mayor es:", elMayor);
 
 // 🧙‍♂️ LA FORMA SENIOR (El Atajo de JS)
 // Una vez que entienden el bucle de arriba, les mostrás este truco de magia:
-// console.log(...numeros);
-// const elMayorSenior = Math.max(...numeros);
-// console.log("Atajo Senior. El mayor es:", elMayorSenior);
+ console.log(...numeros);
+ const elMayorSenior = Math.max(...numeros);
+ console.log("Atajo Senior. El mayor es:", elMayorSenior);
 
 /* =======================================================
    KATA 2: El Patovica del Inventario (Filtrado)
@@ -31,24 +31,24 @@ const tienda = [
   { nombre: "Gorra", precio: 150 },
   { nombre: "Campera", precio: 2500 },
 ];
-// console.log("\n--- KATA 2: FILTRAR INVENTARIO ---");
+ console.log("\n--- KATA 2: FILTRAR INVENTARIO ---");
 
 // 🧑‍💻 LA FORMA JUNIOR (Algoritmo de Filtrado a mano)
-// const productosBaratosManual = []; // Creamos un array vacío (el VIP)
+ const productosBaratosManual = []; // Creamos un array vacío (el VIP)
 
-// for (let i = 0; i < tienda.length; i++) {
-//   let producto = tienda[i];
-//   if (producto.precio < 500) {
-//     productosBaratosManual.push(producto); // Lo dejamos pasar
-//   }
-// }
-// console.log("Filtrado a mano:", productosBaratosManual);
+ for (let i = 0; i < tienda.length; i++) {
+   let producto = tienda[i];
+   if (producto.precio < 500) {
+     productosBaratosManual.push(producto); // Lo dejamos pasar
+   }
+ }
+ console.log("Filtrado a mano:", productosBaratosManual);
 
 // 🧙‍♂️ LA FORMA SENIOR (Usando la máquina .filter)
-// const productosBaratosSenior = tienda.filter(
-//   (producto) => producto.precio < 500,
-// );
-// console.log("Filtrado Senior:", productosBaratosSenior);
+ const productosBaratosSenior = tienda.filter(
+   (producto) => producto.precio < 500,
+ );
+ console.log("Filtrado Senior:", productosBaratosSenior);
 
 /* =======================================================
    KATA 3: La Inflación (Transformación)
@@ -57,26 +57,26 @@ const tienda = [
 console.log("\n--- KATA 3: APLICAR INFLACIÓN ---");
 
 // 🧑‍💻 LA FORMA JUNIOR (Transformación a mano)
-// const tiendaInflacionManual = [];
+ const tiendaInflacionManual = [];
 
-// for (let i = 0; i < tienda.length; i++) {
-//   let productoOriginal = tienda[i];
+ for (let i = 0; i < tienda.length; i++) {
+   let productoOriginal = tienda[i];
 
-//   // Creamos una copia del objeto para no romper el original
-//   let productoModificado = {
-//     nombre: productoOriginal.nombre,
-//     precio: productoOriginal.precio * 1.1,
-//   };
+//  // Creamos una copia del objeto para no romper el original
+   let productoModificado = {
+     nombre: productoOriginal.nombre,
+     precio: productoOriginal.precio * 1.1,
+   };
 
-//   tiendaInflacionManual.push(productoModificado);
-// }
-// console.table(tiendaInflacionManual);
+   tiendaInflacionManual.push(productoModificado);
+ }
+ console.table(tiendaInflacionManual);
 
 // 🧙‍♂️ LA FORMA SENIOR (Usando la máquina .map)
-// const tiendaInflacionSenior = tienda.map((producto) => {
-//   return { nombre: producto.nombre, precio: producto.precio * 1.1 };
-// });
-// console.table(tiendaInflacionSenior);
+ const tiendaInflacionSenior = tienda.map((producto) => {
+   return { nombre: producto.nombre, precio: producto.precio * 1.1 };
+ });
+ console.table(tiendaInflacionSenior);
 
 // /* =======================================================
 //    KATA 4: El Ticket de Compra (Agregación)
@@ -85,16 +85,17 @@ console.log("\n--- KATA 3: APLICAR INFLACIÓN ---");
 console.log("\n--- KATA 4: SUMAR TOTAL ---");
 
 // // 🧑‍💻 LA FORMA JUNIOR (El Acumulador)
-// let totalManual = 0; // La caja registradora arranca en 0
+ let totalManual = 0; // La caja registradora arranca en 0
 
-// for (let i = 0; i < tienda.length; i++) {
-//   totalManual = totalManual + tienda[i].precio;
-// }
-// console.log("Suma a mano: $", totalManual);
+ for (let i = 0; i < tienda.length; i++) {
+   totalManual = totalManual + tienda[i].precio;
+ }
+ console.log("Suma a mano: $", totalManual);
 
-// // 🧙‍♂️ LA FORMA SENIOR (Usando la licuadora .reduce)
-// const totalSenior = tienda.reduce(
-//   (acumulador, producto) => acumulador + producto.precio,
-//   0,
-// );
-// console.log("Suma Senior: $", totalSenior);
+//  //🧙‍♂️ LA FORMA SENIOR (Usando la licuadora .reduce)
+ const totalSenior = tienda.reduce(
+   (acumulador, producto) => acumulador + producto.precio,
+   0,
+ );
+ console.log("Suma Senior: $", totalSenior);
+ 
