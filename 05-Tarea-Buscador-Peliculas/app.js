c726ae2a712157a51d605f66c481c1e016d831de
facuibaquez031997 <facuibaquez03.1997@gmail.com>
2026-04-12 19:26:08 -0300

    termine juego de buscador de peliculas

--- 05-Tarea-Buscador-Peliculas/app.js
+++ 05-Tarea-Buscador-Peliculas/app.js
@@ -1,3 +1,4 @@
+
 // 1. LA BASE DE DATOS MOCK
 const peliculas = [
   { titulo: "El Señor de los Anillos", año: 2001 },
@@ -18,6 +19,14 @@ const inputBusqueda = document.querySelector("#input-busqueda");
 function renderizarPeliculas(arrayPeliculas) {
   // 💡 Tip: Primero limpia el HTML del contenedor usando contenedor.innerHTML = '';
   // TU CÓDIGO AQUÍ 👇
+  contenedor.innerHTML = '';
+  arrayPeliculas.forEach(element => {
+    const tarjeta = document.createElement("div");
+    tarjeta.classList.add("tarjeta");
+    tarjeta.innerHTML = 
+    `<h3>${element.titulo}</h3> 
+    <p>${element.año}</p>`
+  });
 }
 
 // 4. EL BUSCADOR (Event Listener + Filter)
@@ -28,6 +37,8 @@ inputBusqueda.addEventListener("input", (evento) => {
   const textoBuscado = evento.target.value.toLowerCase();
 
   // TU CÓDIGO AQUÍ 👇
+  const peliculasFiltradas = peliculas.filter(pelicula => pelicula.titulo.toLowerCase().includes(textoBuscado));
+  renderizarPeliculas(peliculasFiltradas);
 });
 
 // 5. INICIALIZACIÓN
